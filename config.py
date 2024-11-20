b2f15a57222c234a13bb1696303656c754b52f07
jaden-hong <122812679+jaden-hong@users.noreply.github.com>
2024-11-20 00:10:50 -0500

    basic incomplete data loading

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 #config.py
 
 # folder containing the data
-data_path = ""
+data_path = "/media/jadenh/SSD2/TTC_DATA/"
--- data_load.py
+++ data_load.py
@@ -6,19 +6,39 @@ This code is used to load the data, + process and other stuff idk
 
 import os
 import pandas as pd
+from config import data_path
 
-def loadRawData(type="bus",targets = [], features = []):
+def loadRawData(type="bus",start_year = 2014, end_year = 2015,targets = [], features = []):
     """
     type = bus, subway, streetcar
+    start_year = start of year range
+    end_year = end of year range
     targets = targets of dataset
     features = features of dataset
 
+    
     loads data, based off given parameters
     """
 
-    baseFolder = 
+    subfolder_path = os.path.join(data_path, type)
 
+    if not os.path.isdir(subfolder_path):
+            raise ValueError(f"Subfolder '{type}' does not exist in {data_path}.") #making sure path is correct
+    
+    all_data = pd.DataFrame()
 
+    for filename in os.listdir(subfolder_path):
+        print("On filename:",filename)
+        if (
+            filename.endswith(".xlsx") and
+            filename.startswith(f"ttc-{type}-delay-data") and
+            start_year <= int(filename.split("-")[-1].split(".")[0]) <= end_year
+        ):
+                file_path = os.path.join(subfolder_path, filename)
+                data = pd.read_excel(file_path)
+                all_data = pd.concat([all_data, data], ignore_index=True)    
+
+    all_data.info()
 
 def main():
     loadRawData()
